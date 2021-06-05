19163da82288efc5bd57946cf18199b272b27944
Hrishit Biswas <hrishitbiswas20@gmail.com>
2021-06-06 03:36:14 +0530

    minor fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,17 +8,17 @@ exports.addNewUser = async (req, res, next) => {
 
   try {
     const data = await user.save();
-    res.json(data);
+    res.status(200).json(data);
   } catch (error) {
-    res.send('Error ' + error);
+    res.status(400).send('Error ' + error);
   }
 };
 
 exports.getUsers = async (req, res, next) => {
   try {
     const users = await User.find();
-    res.json(users);
+    res.status(200).json(users);
   } catch (error) {
-    res.send('Error' + error);
+    res.status(400).send('Error' + error);
   }
 };
