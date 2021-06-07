fa2cdb2c0106939ddb9dcec0e94d28fc4b89a719
Abhishek Raj <1828043@kiit.ac.in>
2021-06-07 12:53:17 +0530

    routes mapping changed

--- routes/routes.js
+++ routes/routes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const getUsers = require("./userRoutes/getUsers");
 const addNewUser = require("./userRoutes/addNewUser");
-const userController = require("../controllers/userController");
+const deleteUser = require("./userRoutes/deleteUser");
 
 router.use("/user", getUsers);
 router.use("/user", addNewUser);
-router.delete("/user", userController.delete);
+router.use("/user", deleteUser);
 router.use("/", (req, res, next) => {
   //   console.log(error);
-  
+
   const status = 500;
   const message = "not found";
 
