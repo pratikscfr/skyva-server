29580cd71f819ca67ed3b7d58fb3128cf4bad745
Abhishek Raj <1828043@kiit.ac.in>
2021-06-07 12:54:22 +0530

    minor changes

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const app = require("./app");
 const mongoose = require("mongoose");
 const constants = require("./utils/constants");
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@skyva.rgjb3.mongodb.net/userData?retryWrites=true&w=majority`;
-const uri = "mongodb://localhost:27017/first_db";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@skyva.rgjb3.mongodb.net/userData?retryWrites=true&w=majority`;
+//const uri = "mongodb://localhost:27017/first_db";
 const http = require("http");
 const server = http.createServer(app);
 
