c52798d6971710c3383be1d3068dc0606bf1a589
Hrishit Biswas <hrishitbiswas20@gmail.com>
2021-06-07 18:37:22 +0530

    added get user by email

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const getUsers = require("./userRoutes/getUsers");
-const addNewUser = require("./userRoutes/addNewUser");
-const deleteUser = require("./userRoutes/deleteUser");
+const getUsers = require('./userRoutes/getUsers');
+const addNewUser = require('./userRoutes/addNewUser');
+const deleteUser = require('./userRoutes/deleteUser');
 
-router.use("/user", getUsers);
-router.use("/user", addNewUser);
-router.use("/user", deleteUser);
-router.use("/", (req, res, next) => {
+router.use('/user', getUsers);
+router.use('/user', addNewUser);
+router.use('/user', deleteUser);
+router.use('/', (req, res, next) => {
   //   console.log(error);
 
   const status = 500;
-  const message = "not found";
+  const message = 'not found';
 
   res.status(status).json({ message: message });
 });
