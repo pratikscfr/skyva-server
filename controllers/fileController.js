0c8c7239872a8879421baf67de19adbbf1e6d39a
hrishit biswas <hrishitbiswas20@gmail.com>
2021-06-10 02:36:46 +0530

    uploading bug fix

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -5,6 +5,7 @@ const rootDir = require('../utils/path');
 exports.uploadImg = async (req, res, next) => {
   const image = req.file;
   console.log(image);
+  console.log(rootDir);
   res.sendFile(
     path.join(rootDir, 'uploads', 'images', `test-${image.originalname}`)
   );
