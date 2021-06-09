695b15540d760ad32ad2b5fb4b1ef22adbb615df
hrishit biswas <hrishitbiswas20@gmail.com>
2021-06-09 18:42:24 +0530

    changes minor

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const routes = require('./routes/routes');
 const cors = require('cors');
 express.urlencoded({ extended: false });
 const app = express();
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(cors());
 
 app.use(express.json());
